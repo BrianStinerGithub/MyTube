4fe854d0d0909cf4d5cf0bfe93fdf87936c48cf2
Brian Stiner <Brian.Stiner@revature.net>
2022-04-04 12:38:16 -0700

    setup for project 1

--- 4_4_22.py
+++ 4_4_22.py
@@ -5,3 +5,9 @@ from time import localtime
 if(Path('D:\\').exists()):
     for file in Path('D:\\').glob('**/*.jpg'):
         move(file, Path('C:\\Users\\maste\\OneDrive\\Desktop\\Pictures\\{:%Y-%m-%d}'.format(localtime())))
+
+from threading import Thread
+from multiprocessing import Process
+
+def square(x):
+    yield x * x
