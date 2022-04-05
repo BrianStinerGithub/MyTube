09b11f29b62ae09187f6d81cf20f40798da10bde
Brian Stiner <Brian.Stiner@revature.net>
2022-04-05 13:17:43 -0700

    html templates not notExisting error

--- django/test/test/urls.py
+++ django/test/test/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
+    path('', views.index),
 ]
--- django/test/test/views.py
+++ django/test/test/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import path
 
 
 def hello(request):
-    return render(request, 'test/hello.html', {'name': 'Django'})
+    return render(request, path.abspath('test/hello.html'))
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
