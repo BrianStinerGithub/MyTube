868f6af2afd08bee144c6979b3d58ee633a37fa0
Brian Stiner <Brian.Stiner@revature.net>
2022-04-05 12:59:58 -0700

    django practice

--- django/test/test/views.py
+++ django/test/test/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def hello(request):
-    return HttpResponse('Hello world')
-    
+    return render(request, 'test/hello.html', {'name': 'Django'})
