d289681b6f7a53f8561a028e9c316d8dbb8094a0
Brian Stiner <Brian.Stiner@revature.net>
2022-04-06 12:53:16 -0700

    new project

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,4 +1,4 @@
-"""test URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('', views.index),
 ]
