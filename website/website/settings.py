e14a897724bd53767e97eead4c7a5c8f3121b6aa
Brian Stiner <Brian.Stiner@revature.net>
2022-04-07 12:49:44 -0700

    removed security issue

--- website/website/settings.py
+++ website/website/settings.py
@@ -15,12 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ajfnub^b!p#qkw=wbp9#6(+qfuufb=wt5=mm-!_1h8%cq=jx*i'
+with open( Path.joinpath(BASE_DIR,'./website/etc/secret_key.txt') ) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
