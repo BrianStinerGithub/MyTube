4c4cecd77a7ed8355b941d29e151a27b977fecdc
Brian Stiner <Brian.Stiner@revature.net>
2022-04-07 20:37:34 -0700

    auth0 setup and templates

--- core/Videos/admin.py
+++ core/Videos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.register(models.Video)
+admin.register(models.Channel)
+admin.register(models.Playlist)
+admin.register(models.Comment)
--- core/Videos/models.py
+++ core/Videos/models.py
@@ -1,32 +1,36 @@
+from asyncio.windows_events import NULL
 from unicodedata import category, decimal
 from django.db import models
 
 from core.core.settings import LANGUAGE_CODE
 from uuid import uuid4
 
-class Course(models.Model):
+class Channel(models.Model):
     name =          models.CharField(max_length=255)
-    description =   models.TextField(blank=True)
-    category =      models.CharField(max_length=255, blank=False)
+    about =         models.TextField(blank=True)
+    url =           models.URLField(blank=True)
+    location =      models.CharField(max_length=255, blank=True)
+    category =      models.CharField(max_length=255, blank=True)
     created_at =    models.DateTimeField(auto_now_add=True)
     updated_at =    models.DateTimeField(auto_now=True)
-    author =        models.ForeignKey('auth0.User',on_delete=models.CASCADE)
-    language =      models.CharField(max_length=2,default=LANGUAGE_CODE)
-    image_url =     models.ImageField(upload_to='courses/',blank=True)
-    price =         models.DecimalField(max_digits=6,decimal_places=2)
-    duration =      models.IntegerField()
-    rating =        models.DecimalField(max_digits=3,decimal_places=2)
-    course_uuid =   models.UUIDField(primary_key=True,default=uuid4,editable=False)
-    course_sections=models.ManyToManyField('Section',blank=True)
+    owner =         models.ForeignKey('auth.User',on_delete=models.SET_DEFAULT, default=1)
+    icon =          models.ImageField(upload_to='icons/',blank=True)
+    banner =        models.ImageField(upload_to='banners/',blank=True)
+    subscribers =   models.ManyToManyField('auth.User', related_name='subscribers', blank=True)
+    videos =        models.ManyToManyField('Video', related_name='videos', blank=True)
+    views =         models.IntegerField(default=0)
+    subcriptions =  models.ManyToManyField('Channel', related_name='subscriptions', blank=True)
+    playlists =     models.ManyToManyField('Playlist', related_name='playlists', blank=True)
 
-class Section(models.Model):
-    name =          models.CharField(max_length=255)
-    description =   models.TextField(blank=True)
-    episodes =      models.ManyToManyField('Episode',blank=True)
+class Playlist(models.Model):
+    name =         models.CharField(max_length=255)
+    about =        models.TextField(blank=True)
+    url =          models.URLField(blank=True)
+    videos =       models.ManyToManyField('Video', related_name='playlist_videos', blank=True)
 
-class Episode(models.Model):
+class Video(models.Model):
     name =          models.CharField(max_length=255)
-    file =          models.FileField(upload_to='episodes')
+    file =          models.FileField(upload_to='videos/',blank=False)')
     description =   models.TextField(blank=True)
     duration =      models.IntegerField(max_length=10, decimal_places=0)
     url =           models.URLField(blank=True)
@@ -34,7 +38,7 @@ class Episode(models.Model):
 
 class Comment(models.Model):
     comment =       models.TextField(max_length=1024)
-    author =        models.ForeignKey('auth0.User',on_delete=models.CASCADE)
+    author =        models.ForeignKey('auth.User',on_delete=models.CASCADE)
     created_at =    models.DateTimeField(auto_now_add=True)
     updated_at =    models.DateTimeField(auto_now=True)
-    episode =       models.ForeignKey('Episode',on_delete=models.CASCADE)
+    video =         models.ForeignKey('Video',on_delete=models.SET_DEFAULT, default=1)
