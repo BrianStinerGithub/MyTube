b9c122f4b30531a239f487690bebd5fd1721af04
Brian Stiner <Brian.Stiner@revature.net>
2022-04-07 18:05:59 -0700

    new project from fresh

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,4 +1,4 @@
-"""website URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,21 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('hello/', views.hello, name='hello'),
-]
-urlpatterns += [
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
-    path("callback", views.callback, name="callback"),
-]
-urlpatterns += [
-    path('tictactoe/', include('tictactoe.urls')),
 ]
