b37eeeb1b100fadeee64cce9de8f06488f47030c
Brian Stiner <Brian.Stiner@revature.net>
2022-04-08 15:46:42 -0700

    added views aka total views to channel

--- core/Videos/models.py
+++ core/Videos/models.py
@@ -8,6 +8,7 @@ from uuid import uuid4
 class Channel(models.Model):
     name =          models.CharField(max_length=255)
     about =         models.TextField(blank=True)
+    views =         models.IntegerField(default=0)
     url =           models.URLField(blank=True)
     location =      models.CharField(max_length=255, blank=True)
     category =      models.CharField(max_length=255, blank=True)
@@ -18,7 +19,6 @@ class Channel(models.Model):
     banner =        models.ImageField(upload_to='Videos/Channel/banners/',blank=True)
     subscribers =   models.ManyToManyField('auth.User', related_name='subscribers', blank=True)
     videos =        models.ManyToManyField('Video', related_name='videos', blank=True)
-    views =         models.IntegerField(default=0)
     subcriptions =  models.ManyToManyField('Channel', related_name='subscriptions', blank=True)
     playlists =     models.ManyToManyField('Playlist', related_name='playlists', blank=True)
 
@@ -27,6 +27,10 @@ class Playlist(models.Model):
     about =        models.TextField(blank=True)
     url =          models.URLField(blank=True)
     videos =       models.ManyToManyField('Video', related_name='playlist_videos', blank=True)
+    channel =      models.ForeignKey('Channel', on_delete=models.CASCADE, blank=True, null=True)
+    created_at =   models.DateTimeField(auto_now_add=True)
+    updated_at =   models.DateTimeField(auto_now=True)
+
 
 class Video(models.Model):
     name =          models.CharField(max_length=255)
@@ -35,11 +39,19 @@ class Video(models.Model):
     description =   models.TextField(blank=True)
     duration =      models.IntegerField(default=0)
     url =           models.URLField(blank=True)
+    views =         models.IntegerField(default=0)
+    comments =      models.ManyToManyRel('Comment', related_name='comments', blank=True)
+    channel =       models.ForeignKey('Channel',on_delete=models.SET_DEFAULT, default=1)
+    likes =         models.ManyToManyField('auth.User', related_name='likes', blank=True)
+    dislikes =      models.ManyToManyField('auth.User', related_name='dislikes', blank=True)
     
 
 class Comment(models.Model):
+    video =         models.ForeignKey('Video',on_delete=models.SET_DEFAULT, default=1)
     comment =       models.TextField(max_length=1024)
     author =        models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    likes =         models.ManyToManyField('auth.User', related_name='likes', blank=True)
+    dislikes =      models.ManyToManyField('auth.User', related_name='dislikes', blank=True)
+    replies =       models.ManyToManyField('Comment', related_name='replies', blank=True)
     created_at =    models.DateTimeField(auto_now_add=True)
     updated_at =    models.DateTimeField(auto_now=True)
-    video =         models.ForeignKey('Video',on_delete=models.SET_DEFAULT, default=1)
