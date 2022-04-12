cf40edfeb19422445e1cadf38d35f26bfe079b99
Brian Stiner <Brian.Stiner@revature.net>
2022-04-12 13:42:52 -0700

    updates after meeting

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("profile/<str:username>", views.profile, name="profile"),
     path("watch/<str:video_id>", views.watch_video, name="watch_video"),
     # path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
