0e3260f1a4281a5c754907c21f4f3d3167731c56
Brian Stiner <Brian.Stiner@revature.net>
2022-04-12 10:10:32 -0700

    setting up static files

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("profile/<str:username>", views.profile, name="profile"),
     path("watch/<str:video_id>", views.watch_video, name="watch_video"),
-    path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
-    path("playlist/<str:playlist_id>", views.view_playlist, name="view_playlist"),
-    path("search/<str:query>", views.search, name="search"),
+    # path("channel/<str:channel_id>", views.view_channel, name="view_channel"),
+    # path("playlist/<str:playlist_id>", views.view_playlist, name="view_playlist"),
+    # path("search/<str:query>", views.search, name="search"),
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR /'core'/'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
