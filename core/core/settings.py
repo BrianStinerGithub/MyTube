d8be555facd35623f016407ecd2fbfaf8fc1a976
Brian Stiner <Brian.Stiner@revature.net>
2022-04-13 12:02:55 -0700

    ran collectstatic

--- core/core/settings.py
+++ core/core/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR /'core'/'static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
