f003567021119186cbc32c96b205aa53d00d3457
Brian Stiner <Brian.Stiner@revature.net>
2022-04-12 19:38:04 -0700

    changed var to const and let,tabs

--- core/core/static/custom.js
+++ core/core/static/custom.js
@@ -1,6 +1,6 @@
 function collapse() {
-    var elements = document.getElementsByName('expandable');
-    for(var i = 0; i < elements.length; i++) {
+    const elements = document.getElementsByName('expandable');
+    for(let i = 0; i < elements.length; i++) {
         elements[i].classList.toggle('expanded');
     }
 }
