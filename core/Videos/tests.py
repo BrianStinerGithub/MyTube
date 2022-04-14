6e5d0a158a69091295dc903719655efe12ce516d
aharrah95 <abigail.harrah@revature.net>
2022-04-14 13:58:07 -0500

    Abby first try testing

--- core/Videos/tests.py
+++ core/Videos/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+from core import Videos
+
+
+class ModelsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('127.0.0.1:8000')
+        self.assertEqual(response.status_code, 200)
