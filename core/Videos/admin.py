eab84a41b3db0ba24c3fa1d2f9da0cc22cac6a6b
Brian Stiner <Brian.Stiner@revature.net>
2022-04-14 09:08:14 -0700

    admin.py

--- core/Videos/admin.py
+++ core/Videos/admin.py
@@ -1,8 +1,21 @@
+# Register your models here.
 from django.contrib import admin
 from . import models
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views')
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'owner')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('video', 'author', 'created_at')
+
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'about', 'created_at')
+
 # Register your models here.
-admin.site.register(models.Video)
-admin.site.register(models.Channel)
-admin.site.register(models.Playlist)
-admin.site.register(models.Comment)
+admin.site.register(models.Video, VideoAdmin)
+admin.site.register(models.Channel, ChannelAdmin)
+admin.site.register(models.Playlist, PlaylistAdmin)
+admin.site.register(models.Comment, CommentAdmin)
